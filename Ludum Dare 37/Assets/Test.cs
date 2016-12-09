2954d3dc4a9b63221f5e7d71ed696c4477f3dab3
Seurimas <nicholasanderson5@gmail.com>
2016-12-08 22:10:29 -0600

    Silly stuff\!

---
+++ Ludum Dare 37/Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.position += new Vector3(x, y) * Time.deltaTime * speed;
+	}
+}

0710dff1b422a632c0204da57553ae2bab3f3438
Álvaro Sánchez Díaz <lvaro Snchez Daz>
2016-12-10 12:16:34 -0600

    Added bahamut and behavior according to the pressed arrow key

---
+++ Ludum Dare 37/Assets/BehemotPlayController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehemotPlayController : MonoBehaviour {
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical > 0)
+        {
+            animator.SetInteger("direction", 2);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("direction", 0);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("direction", 1);
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("direction", 3);
+        }
+    }
+}

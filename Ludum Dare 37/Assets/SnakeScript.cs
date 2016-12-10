82d0736e6a163d35ecc6ff5d7fd9633402f88c7a
Seurimas <nicholasanderson5@gmail.com>
2016-12-09 23:15:06 -0600

    Adding a prefab for adventurers.

---
+++ Ludum Dare 37/Assets/SnakeScript.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeScript : MonoBehaviour {
+    public float acceleration = 1;
+    public float maxSpeed = 1;
+    public float rotationSpeed = 1;
+    private bool isMoving = false;
+    private float currentSpeed = 0;
+    private float currentRotation = 0;
+    private float targetRotation = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float delta = Time.deltaTime;
+        updateRotation(delta);
+        updateSpeed(delta);
+        updateSegments(delta);
+	}
+
+    private void updateRotation(float delta)
+    {
+        float difference = Mathf.DeltaAngle(currentRotation, targetRotation);
+        float maxChange = delta * rotationSpeed;
+        if (Math.Abs(difference) < maxChange)
+        {
+            currentRotation = targetRotation;
+        } else if (difference < 0)
+        {
+            currentRotation -= maxChange;
+        } else if (difference > 0)
+        {
+            currentRotation += maxChange;
+        }
+    }
+
+    private void updateSpeed(float delta)
+    {
+        if (isMoving)
+        {
+            currentSpeed += acceleration * delta;
+        } else
+        {
+            currentSpeed -= acceleration * delta;
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+    }
+
+    private void updateSegments(float delta)
+    {
+        Transform head = null;
+        foreach (Transform child in transform)
+        {
+            if (head == null)
+                head = child;
+
+        }
+    }
+
+    public void turnTowards(float target)
+    {
+        targetRotation = target;
+    }
+    public void move()
+    {
+        isMoving = true;
+    }
+    public void stop()
+    {
+        isMoving = false;
+    }
+}

0dc78478905e4f54ce0d4b243265ae42865480a9
Álvaro Sánchez Díaz <lvaro Snchez Daz>
2016-12-10 23:09:10 -0600

    Buggy 'smooth' camera

--- Ludum Dare 37/Assets/CameraController.cs
+++ Ludum Dare 37/Assets/CameraController.cs
@@ -6,14 +6,43 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
     private Vector3 offset;
 
+    float speed = 3;
+    float delta_x;
+    float delta_y;
+    float threshold = 3;
     // Use this for initialization
     void Start () {
 
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+        Debug.Log(transform.position);
+        //transform.position = player.transform.position + offset;
+        if (player.transform.position.x > transform.position.x)
+        {
+            delta_x = player.transform.position.x - transform.position.x;
+        }
+        else
+        {
+            delta_x = transform.position.x - player.transform.position.x;
+        }
+
+        if (player.transform.position.y > transform.position.y)
+        {
+            delta_y = player.transform.position.y - transform.position.y;
+        }
+        else
+        {
+            delta_y = transform.position.y - player.transform.position.y;
+        }
+
+        if(delta_x >= threshold || delta_y >= threshold)
+        {
+            offset = player.transform.position;
+            offset.z = 1;
+            transform.position = Vector3.MoveTowards(transform.position, offset, speed * Time.deltaTime);
+        }
+    }
 }

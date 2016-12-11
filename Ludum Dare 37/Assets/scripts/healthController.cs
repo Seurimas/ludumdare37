7f153e90d4f952ff6b83a3160b9bf18d8bf16b95
Álvaro Sánchez Díaz <heyalvaro@gmail.com>
2016-12-11 09:25:03 -0600

    Rearranged scripts and made enemies invulnerable to weapons

--- Ludum Dare 37/Assets/scripts/healthController.cs
+++ Ludum Dare 37/Assets/scripts/healthController.cs
@@ -33,7 +33,11 @@ public class healthController : MonoBehaviour {
     {
         if (collision.gameObject.tag == "Weapon")
         {
-            health -= collision.gameObject.GetComponent<SwingingWeapon>().weaponDamage;
+            if (gameObject.tag != "Enemy")
+            {
+                health -= collision.gameObject.GetComponent<SwingingWeapon>().weaponDamage;
+            }
+
             if (health < 0)
             {
                 health = 0;

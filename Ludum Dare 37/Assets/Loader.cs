a543d067ec887c8a25233df0f4851fc128115804
Seurimas <nicholasanderson5@gmail.com>
2016-12-10 19:12:03 -0600

    Adding GUI text for gold, timer, and adventurer count.

--- Ludum Dare 37/Assets/Loader.cs
+++ Ludum Dare 37/Assets/Loader.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Loader : MonoBehaviour {
     public GameObject gameManager;
     public List<GameObject> doors;
     public List<GameObject> loots;
+    public Text goldText;
+    public Text timerText;
+    public Text currentText;
     // Use this for initialization
     void Awake() {
         if (GameManager.instance == null)
             Instantiate(gameManager);
         GameManager.instance.adventurerManager.initialize(doors, loots);
+        GameManager.instance.adventurerManager.initializeGUI(timerText, currentText);
+        GameManager.instance.resourceManager.initializeGUI(goldText);
     }
 	
 	// Update is called once per frame

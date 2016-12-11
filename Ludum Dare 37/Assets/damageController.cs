d76eaf25e328e0af07426e01358810d36a1b929a
Seurimas <nicholasanderson5@gmail.com>
2016-12-11 15:12:38 -0600

    Adding noises to various places.

--- Ludum Dare 37/Assets/damageController.cs
+++ Ludum Dare 37/Assets/damageController.cs
@@ -16,6 +16,8 @@ public class damageController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.GetComponent<healthController>().health -= damageDealt;
+        healthController healthController = collision.gameObject.GetComponent<healthController>();
+        if (healthController != null)
+            healthController.health -= damageDealt;
     }
 }

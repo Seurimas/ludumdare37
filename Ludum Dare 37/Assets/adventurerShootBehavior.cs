370e2a19c675b73ce3fc7eb5a6b2d78786d47ddb
Álvaro Sánchez Díaz <heyalvaro@gmail.com>
2016-12-11 18:18:13 -0600

    Added behavior to allow players shooting every 3 seconds. Currently they just shoot downwards

--- Ludum Dare 37/Assets/adventurerShootBehavior.cs
+++ Ludum Dare 37/Assets/adventurerShootBehavior.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class adventurerShootBehavior : MonoBehaviour {
+    public GameObject projectile;
+    private GameObject missile;
     GameObject target;
 	// Use this for initialization
 	void Start () {
         target = GameObject.Find("bahamut");
+        InvokeRepeating("shoot",3.0f,3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Create a vector between adventurer and player to define projectile path
-        Debug.Log(target.transform.position);
-	}
+        Debug.Log(transform.position - target.transform.position);
+        
+    }
+
+    private void shoot()
+    {
+        missile = Instantiate(projectile, new Vector3(0, -1.75f) + transform.position, Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
+        missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1) * missile.GetComponent<projectileController>().speed;
+    }
+
 }
+

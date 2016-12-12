76f201b85d1dba6a9920a8a49429931f198bf561
Álvaro Sánchez Díaz <heyalvaro@gmail.com>
2016-12-11 18:40:51 -0600

    Finished basic shoot script. Whatever object with the script attached will shoot a GameObject to the player every 3 seconds

--- Ludum Dare 37/Assets/adventurerShootBehavior.cs
+++ Ludum Dare 37/Assets/adventurerShootBehavior.cs
@@ -9,20 +9,18 @@ public class adventurerShootBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         target = GameObject.Find("bahamut");
-        InvokeRepeating("shoot",3.0f,3.0f);
+        InvokeRepeating("shoot",0.0f,3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Create a vector between adventurer and player to define projectile path
-        Debug.Log(transform.position - target.transform.position);
-        
     }
 
     private void shoot()
     {
         missile = Instantiate(projectile, new Vector3(0, -1.75f) + transform.position, Quaternion.AngleAxis(180, new Vector3(0, 0, 1)));
-        missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1) * missile.GetComponent<projectileController>().speed;
+        missile.GetComponent<Rigidbody2D>().velocity = (target.transform.position) / Vector3.Distance(transform.position,target.transform.position) * missile.GetComponent<projectileController>().speed;
     }
 
 }

9fb9f4ef34ecc9da5b50b5debbfc0f0694c03737
Álvaro Sánchez Díaz <heyalvaro@gmail.com>
2016-12-11 18:00:57 -0600

    Got bahamut position and printed it to log

--- Ludum Dare 37/Assets/adventurerShootBehavior.cs
+++ Ludum Dare 37/Assets/adventurerShootBehavior.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class adventurerShootBehavior : MonoBehaviour {
-
+    GameObject target;
 	// Use this for initialization
 	void Start () {
-		
+        target = GameObject.Find("bahamut");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Create a vector between adventurer and player to define projectile path
+        Debug.Log(target.transform.position);
 	}
 }

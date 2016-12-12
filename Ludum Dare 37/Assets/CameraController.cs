5bd6471bf4b33e6ce5ab27ffe243dc71c0eec73a
Álvaro Sánchez Díaz <heyalvaro@gmail.com>
2016-12-12 15:14:02 -0600

    Working deadzone camera. Magic number based but it works!

--- Ludum Dare 37/Assets/CameraController.cs
+++ Ludum Dare 37/Assets/CameraController.cs
@@ -9,12 +9,50 @@ public class CameraController : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        if (player != null)
-            transform.position = player.transform.position + offset;
-	}
+        deadzone_check();
+    }
+
+    void deadzone_check()
+    {
+        //MAGIC NUMBERS!
+        int speed = 10;
+        float deltaX = 0.0f;
+        float deltaY = 0.0f;
+        float horizontal_threshold = 5.0f;
+        float vertical_threshold = 2.5f;
+        Vector3 moveTemp;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            deltaX = player.transform.position.x - transform.position.x;
+        }
+        else
+        {
+            deltaX = transform.position.x - player.transform.position.x;
+        }
+
+        if (player.transform.position.y > transform.position.y)
+        {
+            deltaY = player.transform.position.y - transform.position.y;
+        }
+        else
+        {
+            deltaY = transform.position.y - player.transform.position.y;
+        }
+
+        if (deltaX >= horizontal_threshold || deltaY >= vertical_threshold)
+        {
+            moveTemp = player.transform.position;
+            moveTemp.z = -10.0f;
+            transform.position = Vector3.MoveTowards(transform.position, moveTemp, speed * Time.deltaTime);
+        }
+        Debug.Log(transform.position);
+        Debug.Log("deltaX: " + deltaX);
+        Debug.Log("deltaY: " + deltaY);
+    }
+
 }

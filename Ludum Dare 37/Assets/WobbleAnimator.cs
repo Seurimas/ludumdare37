27f60257165f0b5791e0c48fefd3e3d7b71cb184
Seurimas <nicholasanderson5@gmail.com>
2016-12-12 18:29:29 -0600

    Wobbling the heart, since there's no animation.

---
+++ Ludum Dare 37/Assets/WobbleAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WobbleAnimator : MonoBehaviour {
+    private float progress = 0;
+    public float speed = 2;
+    public float intensity = 0.25f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float lastPoint = Mathf.Sin(progress) * intensity;
+        progress += Time.deltaTime * speed;
+        float currentPoint = Mathf.Sin(progress) * intensity;
+        transform.position += new Vector3(0, currentPoint - lastPoint, 0);
+	}
+}
